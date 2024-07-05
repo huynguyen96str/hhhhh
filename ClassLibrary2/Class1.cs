e15fc98c2d5ecc6fafce731464983aeeb6c793e6
huynguyen96str <81135553+huynguyen96str@users.noreply.github.com>
2024-07-05 09:50:17 +0700

    upate

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public void Test()
+        {
+             var workbook = new XLWorkbook();
+            var worksheet = workbook.AddWorksheet("Sample Sheet");
+            worksheet.Cell("A1").Value = "Hello World!";
+            worksheet.Cell("A2").FormulaA1 = "MID(A1, 7, 5)";
+            workbook.SaveAs("HelloWorld.xlsx");
+        }
+
+    }
+}
